c8cd77eed721e0380e794e2d56884d4d4bab0cf0
wmrrrr <150169190+wmrrrr@users.noreply.github.com>
2024-03-08 12:34:48 +0200

    Update test_love.py

--- lab2/test/test_love.py
+++ lab2/test/test_love.py
@@ -7,38 +7,30 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 import unittest
 from src.love import *
 
-class Test(unittest.TestCase):
+class TestFindTripletSum(unittest.TestCase):
     def test1(self):
-        our_list = (1, 2, 3, 4, 6)
-        left = 0
-        right = len(our_list) - 1
-        target = 6
+        our_list = [30, 21, 45, 89, 5, 7, 3, 9, 1]
+        target = 56
         expected_result = True
-        self.assertEqual(binary_search(our_list, left, right, target), expected_result)
+        self.assertEqual(find_triplet_sum(our_list, target), expected_result)
 
     def test2(self):
-        our_list = (1, 2, 3, 4, 6, 7, 9, 10, 5, 8)
-        left = 0
-        right = len(our_list) - 1
-        target = 6
-        expected_result = True
-        self.assertEqual(binary_search(our_list, left, right, target), expected_result)
+        our_list =[20, 40, 88, 6, 8, 33, 9, 5, 4, 50]
+        target = 1
+        expected_result = False
+        self.assertEqual(find_triplet_sum(our_list, target), expected_result)
 
     def test3(self):
-        our_list = (7, 8, 9, 10)
-        left = 0
-        right = len(our_list) - 1
-        target = 6
-        expected_result = False
-        self.assertEqual(binary_search(our_list, left, right, target), expected_result)
+        our_list = [1, 10, 44, 3, 8, 22, 54, 9, 2, 80]
+        target = 58
+        expected_result = True
+        self.assertEqual(find_triplet_sum(our_list, target), expected_result)
 
     def test4(self):
-        our_list = (1, 4, 10, 2, 3, 9)
-        left = 0
-        right = len(our_list) - 1
-        target = 6
-        expected_result = False
-        self.assertEqual(binary_search(our_list, left, right, target), expected_result)        
+        our_list = [1, 10, 44, 3, 8, 22, 54, 9, 2, 80]
+        target = 92
+        expected_result = True
+        self.assertEqual(find_triplet_sum(our_list, target), expected_result)        
 
     
 if __name__ == '__main__':
